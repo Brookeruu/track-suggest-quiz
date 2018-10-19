79e5dcb91703e1054aab0eb6ce00e12532059f5c
Brooke Perry <perrysbrooke@gmail.com>
2018-10-19 10:33:11 -0700

    add js, fix css id to class.

--- js/scripts.js
+++ js/scripts.js
@@ -1,1 +1,33 @@
-var interest = $("#interest").val();
+// var resultInterest = interest(function) {
+//   if (interest === option1) {
+//     return true;
+//   }
+// }
+
+
+//
+// var resultInterest = $("select#interest").val();
+// var longHours
+// var layoutDesign
+// var bigBusiness
+// var describeYou
+
+
+
+
+$(document).ready(function(){
+  $("form#whichTrack").submit(function(event) {
+    var resultInterest = $("select#interest").val();
+
+    if(returnInterest === opt2) {
+      $(".resultNone").show();
+    }
+
+
+
+  event.preventDefault();
+
+
+  })
+
+});
