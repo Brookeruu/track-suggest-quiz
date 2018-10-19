ff56f9335d0792212bd027da4e247e7ca032fb9b
Brooke Perry <perrysbrooke@gmail.com>
2018-10-19 14:19:46 -0700

    add if statement, combine if statements into one  if, else if, statement.

--- js/scripts.js
+++ js/scripts.js
@@ -23,14 +23,40 @@ $(document).ready(function() {
       $("#resultNone").show();
       $("#threeQuestions").hide();
     }
+    //
+    // if(resultLayoutDesign === "1" && resultBigBusiness === "1" || resultLayoutDesign === "1" && resultDescribeYou === "1" || resultBigBusiness === "1" && resultDescribeYou === "1") {
+    //   $("#resultCsharp").show();
+    // }
+    //
+    // if(resultLayoutDesign === "2" && resultBigBusiness === "2" || resultLayoutDesign === "2" && resultDescribeYou === "2" || resultBigBusiness === "2" && resultDescribeYou === "2") {
+    //   $("#resultRuby").show();
+    // }
+    //
+    // if(resultLayoutDesign === "3" && resultBigBusiness === "3" || resultLayoutDesign === "3" && resultDescribeYou === "3" ||  resultBigBusiness === "3" && resultDescribeYou === "3") {
+    //   $("#resultDesign").show();
+    // }
+    //
+    // if(resultLayoutDesign === "1" && resultBigBusiness === "2" && resultDescribeYou === "3" || resultLayoutDesign === "2" && resultBigBusiness === "3" && resultDescribeYou === "1" || resultLayoutDesign === "3" && resultBigBusiness === "1" && resultDescribeYou === "2") {
+    //   $("#resultRuby").show();
+    //   $("#resultCsharp").show();
+    //   $("#resultDesign").show();
+    // }
 
-    if(resultLayoutDesign === "1" && resultBigBusiness === "1" || resultLayoutDesign === "1" && resultDescribeYou === "1" || resultBigBusiness === "1" && resultDescribeYou === "1") {
-      $("#resultCsharp").show();
-    }
 
-    if(resultLayoutDesign === "2" && resultBigBusiness === "2" || resultLayoutDesign === "2" && resultDescribeYou === "2" || resultBigBusiness === "2" && resultDescribeYou === "2") {
-      $("#resultRuby").show();
-    }
+        if(resultLayoutDesign === "1" && resultBigBusiness === "1" || resultLayoutDesign === "1" && resultDescribeYou === "1" || resultBigBusiness === "1" && resultDescribeYou === "1") {
+          $("#resultCsharp").show();
+        } else if(resultLayoutDesign === "2" && resultBigBusiness === "2" || resultLayoutDesign === "2" && resultDescribeYou === "2" || resultBigBusiness === "2" && resultDescribeYou === "2") {
+          $("#resultRuby").show();
+        } else if(resultLayoutDesign === "3" && resultBigBusiness === "3" || resultLayoutDesign === "3" && resultDescribeYou === "3" ||  resultBigBusiness === "3" && resultDescribeYou === "3") {
+          $("#resultDesign").show();
+        } else if(resultLayoutDesign === "1" && resultBigBusiness === "2" && resultDescribeYou === "3" || resultLayoutDesign === "2" && resultBigBusiness === "3" && resultDescribeYou === "1" || resultLayoutDesign === "3" && resultBigBusiness === "1" && resultDescribeYou === "2") {
+          $("#resultRuby").show();
+          $("#resultCsharp").show();
+          $("#resultDesign").show();
+        }
+
+
+
 
 
   });
