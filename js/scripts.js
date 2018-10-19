30705b88266c6c224e51b739cea8dec72a240743
Brooke Perry <perrysbrooke@gmail.com>
2018-10-19 15:31:35 -0700

    add images and adjust css text.

--- js/scripts.js
+++ js/scripts.js
@@ -10,6 +10,9 @@ $(document).ready(function() {
     var resultBigBusiness = $("#bigBusiness").val();
     var resultDescribeYou = $("#describeYou").val();
 
+    var name = $("#userName").val();
+    $("#output").text(name);
+
     if(resultInterest === "Yes") {
       $("#resultNone").hide();
     } else {
@@ -28,18 +31,22 @@ $(document).ready(function() {
         if(resultLayoutDesign === "1" && resultBigBusiness === "1" || resultLayoutDesign === "1" && resultDescribeYou === "1" || resultBigBusiness === "1" && resultDescribeYou === "1") {
           $("#resultCsharp").show();
           $("#extraInfo").show();
+          $("#whichTrack").hide();
         } else if(resultLayoutDesign === "2" && resultBigBusiness === "2" || resultLayoutDesign === "2" && resultDescribeYou === "2" || resultBigBusiness === "2" && resultDescribeYou === "2") {
           $("#resultRuby").show();
           $("#extraInfo").show();
+          $("#whichTrack").hide();
         } else if(resultLayoutDesign === "3" && resultBigBusiness === "3" || resultLayoutDesign === "3" && resultDescribeYou === "3" ||  resultBigBusiness === "3" && resultDescribeYou === "3") {
           $("#resultDesign").show();
           $("#extraInfo").show();
+          $("#whichTrack").hide();
         } else if(resultLayoutDesign === "1" && resultBigBusiness === "2" && resultDescribeYou === "3" || resultLayoutDesign === "2" && resultBigBusiness === "3" && resultDescribeYou === "1" || resultLayoutDesign === "3" && resultBigBusiness === "1" && resultDescribeYou === "2") {
           $("#resultRuby").show();
           $("#resultCsharp").show();
           $("#resultDesign").show();
           $("#extraInfo").show();
           $(".resultAll").show();
+          $("#whichTrack").hide();
         }
 
 
