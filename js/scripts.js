1cbc9832eab06bf95df90b6582abe7aa1f3be7d6
Brooke Perry <perrysbrooke@gmail.com>
2018-10-19 13:14:45 -0700

    add js rule question 2, change syntax html.

--- js/scripts.js
+++ js/scripts.js
@@ -5,16 +5,21 @@ $(document).ready(function() {
     event.preventDefault();
 
     var resultInterest = $("#interest").val();
+    var resultLongHours = $("#longHours").val();
 
     if(resultInterest === "Yes") {
       $("#resultNone").hide();
     } else {
       $("#resultNone").show();
-      
+      $("#threeQuestions").hide();
+    }
+
+    if(resultLongHours === "1") {
+      $("#resultNone").hide();
+    } else {
+      $("#resultNone").show();
+      $("#threeQuestions").hide();
     }
 
   });
-
-
-
 });
