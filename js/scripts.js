59da294d51fadcb8c5c57db83085c8c8022d641f
Brooke Perry <perrysbrooke@gmail.com>
2018-10-27 19:18:34 -0700

    add btn to reset form, clean up js, add css

--- js/scripts.js
+++ js/scripts.js
@@ -10,15 +10,6 @@ $(document).ready(function() {
     var resultBigBusiness = $("#bigBusiness").val();
     var resultDescribeYou = $("#describeYou").val();
 
-    var name = $("#userName").val();
-    $("#output").text(name);
-
-    if(resultInterest === "Yes") {
-      $("#resultNone").hide();
-    } else {
-      $("#resultNone").show();
-      $("#threeQuestions").hide();
-    }
 
     if(resultLongHours === "1") {
       $("#resultNone").hide();
@@ -27,54 +18,34 @@ $(document).ready(function() {
       $("#threeQuestions").hide();
     }
 
-        if(resultLayoutDesign === "1" && resultBigBusiness === "1" || resultLayoutDesign === "1" && resultDescribeYou === "1" || resultBigBusiness === "1" && resultDescribeYou === "1") {
-          $("#resultCsharp").show();
-          $("#extraInfo").show();
-          $("#whichTrack").hide();
-          $("h1").hide();
-        } else if(resultLayoutDesign === "2" && resultBigBusiness === "2" || resultLayoutDesign === "2" && resultDescribeYou === "2" || resultBigBusiness === "2" && resultDescribeYou === "2") {
-          $("#resultRuby").show();
-          $("#extraInfo").show();
-          $("#whichTrack").hide();
-            $("h1").hide();
-        } else if(resultLayoutDesign === "3" && resultBigBusiness === "3" || resultLayoutDesign === "3" && resultDescribeYou === "3" ||  resultBigBusiness === "3" && resultDescribeYou === "3") {
-          $("#resultDesign").show();
-          $("#extraInfo").show();
-          $("#whichTrack").hide();
-            $("h1").hide();
-        } else if(resultLayoutDesign === "1" && resultBigBusiness === "2" && resultDescribeYou === "3" || resultLayoutDesign === "2" && resultBigBusiness === "3" && resultDescribeYou === "1" || resultLayoutDesign === "3" && resultBigBusiness === "1" && resultDescribeYou === "2") {
-          $("#resultRuby").show();
-          $("#resultCsharp").show();
-          $("#resultDesign").show();
-          $("#extraInfo").show();
-          $(".resultAll").show();
-          $("#whichTrack").hide();
-          $("h1").hide();
-        }
+    if(resultLayoutDesign === "1" && resultBigBusiness === "1" || resultLayoutDesign === "1" && resultDescribeYou === "1" || resultBigBusiness === "1" && resultDescribeYou === "1") {
+      $("#resultCsharp").show();
+      $("#extraInfo").show();
+      $("#whichTrack").hide();
+      // $("h1").hide();
+    } else if(resultLayoutDesign === "2" && resultBigBusiness === "2" || resultLayoutDesign === "2" && resultDescribeYou === "2" || resultBigBusiness === "2" && resultDescribeYou === "2") {
+      $("#resultRuby").show();
+      $("#extraInfo").show();
+      $("#whichTrack").hide();
+      // $("h1").hide();
+    } else if(resultLayoutDesign === "3" && resultBigBusiness === "3" || resultLayoutDesign === "3" && resultDescribeYou === "3" ||  resultBigBusiness === "3" && resultDescribeYou === "3") {
+      $("#resultDesign").show();
+      $("#extraInfo").show();
+      $("#whichTrack").hide();
+      // $("h1").hide();
+    } else if(resultLayoutDesign === "1" && resultBigBusiness === "2" && resultDescribeYou === "3" || resultLayoutDesign === "2" && resultBigBusiness === "3" && resultDescribeYou === "1" || resultLayoutDesign === "3" && resultBigBusiness === "1" && resultDescribeYou === "2") {
+      $("#resultRuby").show();
+      $("#resultCsharp").show();
+      $("#resultDesign").show();
+      $("#extraInfo").show();
+      $(".resultAll").show();
+      $("#whichTrack").hide();
+      // $("h1").hide();
+    }
+  });
+
+  $("#resetBtn").click(function(event) {
+    event.preventDefault();
+    location.reload();
   });
 });
-
-
-
-
-// Below is the individual if statements for each individual programming language result:
-
-
-//
-// if(resultLayoutDesign === "1" && resultBigBusiness === "1" || resultLayoutDesign === "1" && resultDescribeYou === "1" || resultBigBusiness === "1" && resultDescribeYou === "1") {
-//   $("#resultCsharp").show();
-// }
-//
-// if(resultLayoutDesign === "2" && resultBigBusiness === "2" || resultLayoutDesign === "2" && resultDescribeYou === "2" || resultBigBusiness === "2" && resultDescribeYou === "2") {
-//   $("#resultRuby").show();
-// }
-//
-// if(resultLayoutDesign === "3" && resultBigBusiness === "3" || resultLayoutDesign === "3" && resultDescribeYou === "3" ||  resultBigBusiness === "3" && resultDescribeYou === "3") {
-//   $("#resultDesign").show();
-// }
-//
-// if(resultLayoutDesign === "1" && resultBigBusiness === "2" && resultDescribeYou === "3" || resultLayoutDesign === "2" && resultBigBusiness === "3" && resultDescribeYou === "1" || resultLayoutDesign === "3" && resultBigBusiness === "1" && resultDescribeYou === "2") {
-//   $("#resultRuby").show();
-//   $("#resultCsharp").show();
-//   $("#resultDesign").show();
-// }
